917800f30f7cfe29ce9d1ec84348bde22fa4d6a5
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-19 19:12:48 +0530

    Refactored: To Create Generic Class

--- src/com/bridgelab/FindMaximum.java
+++ src/com/bridgelab/FindMaximum.java
@@ -1,19 +1,30 @@
 package com.bridgelab;
 
-public class FindMaximum {
-	    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+public class FindMaximum<T extends Comparable<T>> {
+	
+		T a1, a2, a3;
+
+		public FindMaximum(T a1, T a2, T a3) {
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+		
+		}
+		public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
 		T max = a1;
 		if (a2.compareTo(max) > 0)
-			max = a2;
+		max = a2;
 		if (a3.compareTo(max) > 0)
-			max = a3;
+		max = a3;
 		return max;
-	}
+		}
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		System.out.println("Welcome To Find Maximun Problem Using Generics \n");
 		System.out.println("The maximum value between the three integer is : " + maxOfValues(15, 87, 68)+ "\n");
 		System.out.println("The maximum value between the three float is : " + maxOfValues(1.5f, 8.7f, 6.8f) + "\n");
 		System.out.println("The maximum value between the three string is : " + maxOfValues("Apple", "Banana", "Peach"));
+		}
 	}
-}
+
+	
